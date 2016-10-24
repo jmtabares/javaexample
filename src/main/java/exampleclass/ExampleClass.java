562e65b0ef0cddcebacf7e6ce002ba027d7d1801
jtabaresGAP <jtabares@growthaccelerationpartners.com>
2016-10-24 07:00:23 -0300

    initial Commit
    initial Commit KT session02

---
+++ src/main/java/exampleclass/ExampleClass.java
@@ -0,0 +1,20 @@
+package exampleclass;
+
+/**
+ * Created by jairtabares on 24/10/16.
+ */
+public class ExampleClass {
+    public ExampleClass(){
+
+    }
+    public Float sum(Float termA, Float termB){
+        Float result =  termA+termB;
+        return result;
+
+    }
+    public Float divide(Float termA, Float termB){
+        Float result =  termA/termB;
+        return result;
+
+    }
+}
---
+++ src/test/java/exampleclass/ExampleClassTest.java
@@ -0,0 +1,32 @@
+package exampleclass;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by jairtabares on 24/10/16.
+ */
+public class ExampleClassTest {
+    @Test(description = "Test a sum",
+            groups = {"test1"})
+    @Parameters(value={"termA","termB","resultSum1"})
+    public void testSum(Float termA, Float termB, Float expected) throws Exception {
+        ExampleClass exampleClass =  new ExampleClass();
+        Float toVerify = exampleClass.sum(termA,termB);
+        Assert.assertTrue(toVerify.equals(expected)); //para comparar 2 objetos por el valor, el equals es lo necesario. utilizar
+        //el operador == implica validar que los objtos on iguales, no solo el valor
+    }
+
+    @Test(description = "Divide test",
+            groups = {"test1"})
+    @Parameters(value={"termB","termC","resultDivide"})
+    public void testDivide(Float termA, Float termB, Float expected) throws Exception {
+        ExampleClass exampleClass =  new ExampleClass();
+        Float toVerify = exampleClass.divide(termA,termB);
+        Assert.assertTrue(toVerify.equals(expected));
+    }
+
+}
